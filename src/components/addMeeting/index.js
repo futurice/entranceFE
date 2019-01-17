f2006bd60817fdbd0d6b776b45d8055bfbedb881
Stefan Milenkovic <stefan.milenkovic@futurice.com>
2019-01-17 11:08:37 +0100

    changes to FE

--- src/components/addMeeting/index.js
+++ src/components/addMeeting/index.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
 import './style.css';
 
 class AddMeeting extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			host: '',
-			meeting : '',
-			room: ''
+			meeting: {
+				host: '',
+				phone: '',
+				meeting: '',
+				room: '',
+				date: this.newDateToString()
+			},
+			added: false
 		}
 	}
 
@@ -17,15 +23,34 @@ class AddMeeting extends Component {
 				<div className="add-meeting-form">
 					<div>
 						<p className="add-meeting-label"> You are? </p>
-						<input className="add-meeting-input" onChange={evt => this.updateHost(evt)}/>
+						<input id="host" className="add-meeting-input" value={this.state.meeting.host} onChange={evt => this.updateMeeting(evt)}/>
+					</div>
+					<div>
+						<p className="add-meeting-label"> Your phone? </p>
+						<input id="phone" className="add-meeting-input" value={this.state.meeting.phone} onChange={evt => this.updateMeeting(evt)} />
 					</div>
 					<div>
 						<p className="add-meeting-label"> Meeting name? </p>
-						<input className="add-meeting-input" onChange={evt => this.updateMeeting(evt)}/>
+						<input id="meeting" className="add-meeting-input" value={this.state.meeting.meeting} onChange={evt => this.updateMeeting(evt)}/>
 					</div>
 					<div>
 						<p className="add-meeting-label"> Meeting room? </p>
-						<input className="add-meeting-input" onChange={evt => this.updateRoom(evt)}/>
+						<input id="room" className="add-meeting-input" value={this.state.meeting.room} onChange={evt => this.updateMeeting(evt)}/>
+					</div>
+					<div>
+						<p className="add-meeting-label"> Date/time ? </p>
+						<div className="datetime-picker-wrap">
+							<TextField
+								id="datetime-local"
+								type="datetime-local"
+								defaultValue={this.newDateToString()}
+								className="datetime-picker"
+								InputLabelProps={{
+									shrink: true,
+								}}
+								onChange={evt => this.updateMeeting(evt)}
+							/>
+						</div>
 					</div>
 				</div>
 				<div className="add-button-wrapper">
@@ -35,32 +60,42 @@ class AddMeeting extends Component {
 		);
 	}
 
-	updateHost(evt) {
-		console.log(this.state);
-		this.setState({
-			host: evt.target.value
-		})
-	}
 	updateMeeting(evt) {
-		console.log(this.state);
-		this.setState({
-			meeting: evt.target.value
-		})
+		let { meeting } = this.state;
+
+		switch(evt.target.id) {
+			case 'host': meeting.host = evt.target.value; break;
+			case 'phone': meeting.phone = evt.target.value; break;
+			case 'meeting': meeting.meeting = evt.target.value; break;
+			case 'room': meeting.room = evt.target.value; break;
+			case 'datetime-local': meeting.date = evt.target.value; break;
+		}
+		this.setState({ meeting });
 	}
-	updateRoom(evt) {
-		console.log(this.state);
-		this.setState({
-			room: evt.target.value
-		})
-	}
+
 	addMeeting(event){
 		event.preventDefault();
+		console.log(this.state);
 		fetch('http://localhost:8000/meetings', {
 			method: 'post',
 			headers: {'Content-Type':'application/json'},
-			body: JSON.stringify(this.state)
+			body: JSON.stringify(this.state.meeting)
+		}).then(() => {
+			this.setState({
+				meeting: {
+					host: '',
+					phone: '',
+					meeting: '',
+					room: '',
+					date: this.newDateToString()
+				}
+			})
 		});
 	};
+
+	newDateToString() {
+		return new Date().toISOString().split('.')[0].slice(0, -3)
+	}
 }
 
 
