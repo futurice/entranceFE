9eb2b95b557534c605769a8cff4c4ad4c53ba678
Yannick Scherer <yannick.scherer@futurice.com>
2019-03-06 12:27:41 +0100

    Add configuration module

---
+++ src/config.js
@@ -0,0 +1,27 @@
+const env = (variableBase, defaultValue) => {
+  const variable = `REACT_APP_${variableBase}`;
+  const value = process.env[variable] || defaultValue;
+
+  if (typeof value === 'undefined') {
+    throw new Error(`Environment variable '${variable}' is required!`);
+  }
+
+  return value;
+};
+
+// !!! NOTE !!!
+//
+// Environment variables have to be supplied as `REACT_APP_<variable>` due to
+// 'create-react-app' ignoring everything else.
+//
+// See:
+// - https://medium.com/@tacomanator/environments-with-create-react-app-7b645312c09d
+// - https://github.com/facebook/create-react-app/issues/2865
+//
+// The shorthand here is just for readability!
+//
+export default {
+  backend: {
+    url: env('BACKEND_URL', 'http://localhost:8000'),
+  },
+};
